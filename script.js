8a6dfb31336c3c0011eed62661c56cb4fbe04ea0
san <san@pop-os.localdomain>
2021-12-04 19:55:32 +0100

    html parcer

---
+++ script.js
@@ -0,0 +1,77 @@
+    /*alert("ta" + " " + "ralhayat" + " " + "machi" + " " + "sahla") 
+
+  var rase ={first: "san",
+            secound: "razor",
+            thied:"ahmed",
+            fourth:"sady"};
+    var rase = {};
+    rase.first = "san"
+  alert(rase.first)
+ var aa = 10, ss = 20;
+  if(aa>=ss){
+    alert("good");
+  }
+  else{
+    alert("bad")
+  }*/
+
+                                                  //[&:and, |:or]
+
+/*var myName = "sady";
+
+switch(myName){
+         case "san":
+         alert(1);
+         break;
+         case "razor":
+         alert(2);
+         break;
+         case "sady":
+         alert(3);
+         break;
+}
+
+var a = 1;
+while(a<=1000){
+      document.getElementById("san").innerHTML += "number:" + " " + a +"<br/>";
+      a++
+}
+
+for(var a = 0;a<=20; a++){
+    document.getElementById("san").innerHTML += a + "<br/>";
+}
+
+var a = 200;
+do{
+  document.getElementById("san").innerHTML += a + "<br/>";
+  a--;
+}while(a>=0)  
+
+function test(){
+  var name1 = "mokhtar";
+  if(name1 ){
+    alert("hello " + name1);
+  }
+}
+
+function names(number){
+  alert(number);
+}
+names(25)
+
+
+
+function test(salary){
+        var x = 100;
+        return salary + x;
+}
+var y = prompt("claculate age in days");
+document.getElementById("san").innerHTML = test(y);
+
+*/
+
+function input(name1){
+  var name1 = document.getElementById("textarea").value;
+  document.getElementById("output").innerHTML = name1;
+}
+
