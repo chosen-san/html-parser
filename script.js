b328c55cfea1ad26a5b3657a19ccd4a817ad9605
san <san@pop-os.localdomain>
2021-12-08 23:04:45 +0100

    final

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-    /*alert("ta" + " " + "ralhayat" + " " + "machi" + " " + "sahla") 
+    /*
+    
+    alert("ta" + " " + "ralhayat" + " " + "machi" + " " + "sahla") 
 
   var rase ={first: "san",
             secound: "razor",
@@ -17,7 +19,8 @@
 
                                                   //[&:and, |:or]
 
-/*var myName = "sady";
+/*
+var myName = "sady";
 
 switch(myName){
          case "san":
@@ -68,10 +71,64 @@ function test(salary){
 var y = prompt("claculate age in days");
 document.getElementById("san").innerHTML = test(y);
 
+
+
+
+
+
+window.onload = function test(){
+  document.getElementById("san").innerHTML = "welcome to my world"
+}
+
+
+
+window.onclick = function test(){
+  document.getElementById("san").innerHTML = "welcome to my world"
+}
+
+
+window.ondblclick = function test(){
+  document.getElementById("san").innerHTML = "welcome to my world"
+}
+
+
+ 
+var test = "ana wa3ar ",
+    test2 = test.substr(4,5);
+console.log(test2);
+console.log(typeof (test2));
+
+
+
+var test = "ana rany jay bjahd akhawti",
+    test2 = test.trim();
+alert(test2);
+
+
+
+var test = Math.round(7.1)
+console.log(test);
+
+var test = Math.ceil(7.1)
+console.log(test);
+
+
+var test = Math.floor(7.1)
+console.log(test);
+
+
+var test = Math.min(7.1)               // (max :kadir l3aks)
+console.log(test);                     // (random : katala3 ra9m 3achwa2i )
+//                   var test = Math.random();
+//                   console.log(Math.ceil(test * 5))
+
+
+
+var test = new Date("jan 01 2002 00:00:");
+console.log(test)
 */
 
 function input(name1){
   var name1 = document.getElementById("textarea").value;
   document.getElementById("output").innerHTML = name1;
 }
-
